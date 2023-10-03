a66c073a19b94d15b66a1e069c2638d43995d321
Kevin <kevinolongo9@gmail.com>
2023-10-03 18:36:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 // Add your code here
+function submitData(name, email){
+    //Sending Data using POST
+fetch("http://localhost:3000/users",{
+    method: "POST",
+    headers: {"Content-Type": "application/json",
+    Accept: "application/json",
+},
+body:JSON.stringify({
+    name:"Steve",
+    email:"steve@steve.com",
+})
+
+})
+
+    //Handling Response
+.then(function (response){
+    return response.json();
+})
+.then(function (object){
+    console.log(object)
+});
+
+    //Handle Errors
+
+
+}
+// const submitData ={
+//     userName:"",
+//     userEmail:"",
+// };
+
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email,
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(userData),
+    };
+  
+    return fetch('http://localhost:3000/users', configurationObject)
+      .then(response => response.json())
+      .then(data => {
+        const idElement = document.createElement('p');
+        idElement.textContent = data.id;
+        document.body.appendChild(idElement);
+      })
+      .catch(error => {
+        const errorElement = document.createElement('p');
+        errorElement.textContent = error.message;
+        document.body.appendChild(errorElement);
+      });
+  }
+  
+  // You can call the submitData function with a name and email as follows:
+  // Replace 'John' and 'john@example.com' with the desired values
+  
+  submitData('Basil', 'basil@gmail.com');
+  
+  
